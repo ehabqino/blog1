7b44931af9b91f81db15d24d3df032ee9b77313b
ehab qino <ehabqino@gmail.com>
2021-07-03 07:40:21 +0300

    commit

---
+++ src/js/models/article.model.js
@@ -0,0 +1,11 @@
+define(['ojs/ojcore','jquery','knockout'], 
+    function() {
+        class Article {
+            constructor(){
+
+            }//end constructor
+
+        }//end class
+    return new Article();
+    
+});//end all
---
+++ src/js/models/class.model.js
@@ -0,0 +1,14 @@
+define(['ojs/ojcore','jquery','knockout'], 
+    function() {
+        class Classification {
+            constructor(){
+                this.id=0;
+                this.title="No Title Yet";
+                this.description="No Description yet"
+
+            }//end constructor
+
+        }//end class
+    return new Classification();
+    
+});//end all
